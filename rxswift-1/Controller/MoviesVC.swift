c04f5f5b321eafecf4e1b62e35a6a832f7e91fee
McL <mcl@brm>
2018-09-27 12:46:32 +0300

    table view is bound to moviesVC

--- rxswift-1/Controller/MoviesVC.swift
+++ rxswift-1/Controller/MoviesVC.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MoviesVC: UITableViewController {
+    
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
---
+++ rxswift-1/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  rxswift-1
+//
+//  Created by McL on 9/27/18.
+//  Copyright © 2018 McL. All rights reserved.
+//
+
+import Foundation
+
